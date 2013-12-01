75318066915764107b4e6485ee80bc5ba171fe9c
Ville Kojola <ville.kojola@gmail.com>
2013-12-01 16:16:11 +0200

    Added template for onSaveInstanceState callback function.

--- restaurantmenu/src/uni/oulu/hci/restaurantmenu/MainMenuActivity.java
+++ restaurantmenu/src/uni/oulu/hci/restaurantmenu/MainMenuActivity.java
@@ -2,6 +2,7 @@ package uni.oulu.hci.restaurantmenu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 
@@ -20,6 +21,19 @@ public class MainMenuActivity extends Activity {
         return true;
     }
     
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        
+    	// To save the state of an activity onSaveInstanceState must be overridden to allow for more information to be saved
+    	
+    	// Save the user's current game state
+        //savedInstanceState.putInt(STATE_SCORE, mCurrentScore);
+        //savedInstanceState.putInt(STATE_LEVEL, mCurrentLevel);
+        
+        // Always call the superclass so it can save the view hierarchy state
+        //super.onSaveInstanceState(savedInstanceState);
+    }
+    
     public void languagesClicked(final View view) {
     	setResult(RESULT_OK, this.getIntent());
     	finish();
@@ -28,6 +42,8 @@ public class MainMenuActivity extends Activity {
     public void checkoutClicked(final View view) {
     	// should be active after there are dishes or drinks that have been paid
     	// launch activity payment options
+    	//Intent intent = new Intent(this, CheckoutActivity.class);
+    	//startActivityForResult(intent, 0);
     }
     
     public void myOrderClicked(final View view) {
