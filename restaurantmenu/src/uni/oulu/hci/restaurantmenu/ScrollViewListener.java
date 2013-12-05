d366742da404209acf9ebd989bd4742ec8c65461
Miikka Salmi <mitusalmi@gmail.com>
2013-12-05 21:21:13 +0200

    Abandoned opening items when scrolling, now opened only by user click.

--- restaurantmenu/src/uni/oulu/hci/restaurantmenu/ScrollViewListener.java
+++
@@ -1,8 +0,0 @@
-package uni.oulu.hci.restaurantmenu;
-
-/**
- * http://stackoverflow.com/questions/3948934/synchronise-scrollview-scroll-positions-android
- */
-public interface ScrollViewListener {
-	void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy);
-}
