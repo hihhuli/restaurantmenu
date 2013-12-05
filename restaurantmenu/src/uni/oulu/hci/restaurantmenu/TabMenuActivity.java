5d8c969b54529dde12ed70eddabdd2e40d852a68
Miikka Salmi <mitusalmi@gmail.com>
2013-12-05 14:33:33 +0200

    Scrolling should now work properly so that the expanded window is updated constantly

--- restaurantmenu/src/uni/oulu/hci/restaurantmenu/TabMenuActivity.java
+++ restaurantmenu/src/uni/oulu/hci/restaurantmenu/TabMenuActivity.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
@@ -20,7 +21,7 @@ public class TabMenuActivity extends Activity implements ScrollViewListener {
     
     private PopupWindow searchpopup;
     private List<MenuItem> menuItems;
-    private ViewGroup expandedItem;
+    private int expandedIndex;
     
     @SuppressWarnings("unchecked")
     @Override
@@ -30,35 +31,53 @@ public class TabMenuActivity extends Activity implements ScrollViewListener {
         
         this.menuItems = (List<MenuItem>) getIntent().getSerializableExtra("data");
         this.searchpopup = null;
-        this.expandedItem = null;
+        this.expandedIndex = 1;
         
         ((ObservableScrollView) findViewById(R.id.scrollView)).setScrollViewListener(this);
         populateScrollView();
     }
     
     private void populateScrollView() {
-    	ViewGroup item;
-    	LinearLayout scrollViewLayout = (LinearLayout)findViewById(R.id.scrollViewLayout);
+    	ViewGroup layout;
         
         for (int i = 0; i < this.menuItems.size(); i++) {
-        	MenuItem menuItem = this.menuItems.get(i);
         	if (i == 1) {
-        		item = (GridLayout)getLayoutInflater().inflate(R.layout.largeitem, null);
-        		this.expandedItem = item;
-        		((TextView)item.findViewById(R.id.itemDescriptionView)).setText(menuItem.getDescription());
-        		((TextView)item.findViewById(R.id.itemSpicinessView)).setText("Spiciness: " + menuItem.getSpiciness());
-        		((TextView)item.findViewById(R.id.itemDietsView)).setText("Diets: " + menuItem.getDiets());
+        		layout = getExpandedItemLayout(i);
         	} else {
-        		item = (LinearLayout)getLayoutInflater().inflate(R.layout.smallitem, null);
-                ((TextView)item.findViewById(R.id.itemPriceView)).setText(Double.toString(menuItem.getPrice()));
-                ((TextView)item.findViewById(R.id.itemDietsView)).setText(menuItem.getDiets());
+        		layout = getItemLayout(i);
         	}
-            ((TextView)item.findViewById(R.id.itemTitleView)).setText(menuItem.getTitle());
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            params.setMargins(0, 0, 0, 10);
-            item.setLayoutParams(params);
-            scrollViewLayout.addView(item);
+            insertToScrollView(layout, ((LinearLayout)findViewById(R.id.scrollViewLayout)), i);
         }
+    }
+    
+    private void insertToScrollView(View view, LinearLayout scrollViewLayout, int index) {
+    	LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(0, 0, 0, 10);
+        view.setLayoutParams(params);
+    	scrollViewLayout.addView(view, index);
+    }
+    
+    private GridLayout getExpandedItemLayout(int index) {
+    	MenuItem menuItem = this.menuItems.get(index);
+    	GridLayout layout = (GridLayout)getLayoutInflater().inflate(R.layout.largeitem, null);
+		
+    	((TextView)layout.findViewById(R.id.itemDescriptionView)).setText(menuItem.getDescription());
+		((TextView)layout.findViewById(R.id.itemSpicinessView)).setText("Spiciness: " + menuItem.getSpiciness());
+		((TextView)layout.findViewById(R.id.itemDietsView)).setText("Diets: " + menuItem.getDiets());
+        ((TextView)layout.findViewById(R.id.itemTitleView)).setText(menuItem.getTitle());
+		
+		return layout;
+    }
+    
+    private LinearLayout getItemLayout(int index) {
+    	MenuItem menuItem = this.menuItems.get(index);
+    	LinearLayout layout = (LinearLayout)getLayoutInflater().inflate(R.layout.smallitem, null);
+    	
+        ((TextView)layout.findViewById(R.id.itemPriceView)).setText(Double.toString(menuItem.getPrice()));
+        ((TextView)layout.findViewById(R.id.itemDietsView)).setText(menuItem.getDiets());
+        ((TextView)layout.findViewById(R.id.itemTitleView)).setText(menuItem.getTitle());
+        
+        return layout;
     }
     
     @Override
@@ -106,8 +125,40 @@ public class TabMenuActivity extends Activity implements ScrollViewListener {
         Log.d("TabMenuActivity","Clear button clicked.");
     }
 
+    private boolean isVisibleInScrollView(View view, ObservableScrollView scrollView) {
+		Rect scrollBounds = new Rect();
+		scrollView.getHitRect(scrollBounds);
+    	if (view.getLocalVisibleRect(scrollBounds)) {
+		    return true;
+		} else {
+			return false;
+		}
+    }
+    
+    private void updateExpandedItem(LinearLayout scrollViewLayout, ObservableScrollView scrollView) {
+    	int i;
+		for (i = 0; i < scrollViewLayout.getChildCount(); i++) {
+			if (isVisibleInScrollView(scrollViewLayout.getChildAt(i), scrollView)) {
+				break;
+			}
+		}
+    	scrollViewLayout.removeViewAt(this.expandedIndex);
+		insertToScrollView(getItemLayout(this.expandedIndex), scrollViewLayout, this.expandedIndex);
+		this.expandedIndex = i + 1;
+		scrollViewLayout.removeViewAt(this.expandedIndex);
+		insertToScrollView(getExpandedItemLayout(this.expandedIndex), scrollViewLayout, this.expandedIndex);
+    }
+    
 	@Override
 	public void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy) {
-		Log.d("onScroll", x + " " + y + " " + oldx + " " + oldy);
+		LinearLayout scrollViewLayout = (LinearLayout) scrollView.getChildAt(0);
+		if (this.expandedIndex > 0 && 
+				this.expandedIndex < scrollViewLayout.getChildCount() - 1 &&
+				!isVisibleInScrollView(scrollViewLayout.getChildAt(this.expandedIndex - 1), scrollView)) {
+			updateExpandedItem(scrollViewLayout, scrollView);
+		} else if (this.expandedIndex > 1 && 
+				isVisibleInScrollView(scrollViewLayout.getChildAt(this.expandedIndex - 2), scrollView)) {
+			updateExpandedItem(scrollViewLayout, scrollView);
+		}
 	}
 }
